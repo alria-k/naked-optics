9fa6d2e734a941c39971160c1edfdc283cc00791
Alria <artur.kirovich@gmail.com>
2022-11-28 21:45:18 +0600

    everything for mouse event on nav ended (html/css) starting js dynamic

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 // burger menu
 
 let burger = document.querySelector(".burger__menu"),
-  burgerMenu = document.querySelector(".mobile-nav__box");
+  burgerLines = document.querySelector(".burger-lines"),
+  burgerMenu = document.querySelector(".mobile-nav__box"),
+  overflow = document.createElement("div");
+
+overflow.className = "overflow-popup";
 
 burger.addEventListener("click", (e) => {
   burgerMenu.classList.toggle("hiden");
+  burgerLines.classList.toggle("burger-cross");
   if (burgerMenu.classList.contains("hiden")) {
     document.body.style.overflow = "visible";
-    document.body.style.background = "#F6F6F6";
+    overflow.remove();
   } else {
     document.body.style.overflow = "hidden";
-    document.body.style.background = "rgba(34, 60, 80, 0.2)";
+    document.body.prepend(overflow);
   }
 });
