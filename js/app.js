9353a12bb086a6a2a70909a59835c775be2dbf51
Alria <artur.kirovich@gmail.com>
2022-11-30 21:52:46 +0600

    tabs opening by hover ended, making view pretier

--- js/app.js
+++ js/app.js
@@ -18,3 +18,31 @@ burger.addEventListener("click", (e) => {
     document.body.prepend(overflow);
   }
 });
+
+// mouse enter nav
+
+let navItem = document.querySelectorAll(".catalog-object"),
+  catalogBox = document.querySelectorAll(".header__catalog");
+
+navItem.forEach((nav) => {
+  nav.onmouseenter = nav.onmouseleave = showAndHideCatalog;
+});
+catalogBox.forEach((catal) => {
+  catal.onmouseleave = showAndHideCatalog;
+});
+
+function showAndHideCatalog(event) {
+  let catalogItem = event.currentTarget
+    .closest(".nav__item")
+    .querySelector(".header__catalog");
+  if (event.type == "mouseenter" && catalogItem.classList.contains("hiden")) {
+    catalogItem.classList.remove("hiden");
+    document.body.prepend(overflow);
+  } else if (
+    event.type == "mouseleave" &&
+    !event.relatedTarget.classList.contains("nav__item")
+  ) {
+    catalogItem.classList.add("hiden");
+    overflow.remove();
+  }
+}
